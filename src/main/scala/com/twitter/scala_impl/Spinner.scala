caed4657ea1aeae2bbf8109ac10e27ee5c027b7e
Carsten Varming <cvarming@twitter.com>
2015-03-18 15:49:20 -0400

    Limit cpu hog to one hour

--- src/main/scala/com/twitter/scala_impl/Spinner.scala
+++ src/main/scala/com/twitter/scala_impl/Spinner.scala
@@ -3,20 +3,24 @@ package com.twitter.scala_impl
 import scala.annotation.tailrec
 
 object Spinner {
-
   val Billion: Long = 1000L * 1000L * 1000L
 
   @tailrec
-  def spin(helloTime: Long): Unit = {
-    if (helloTime < System.nanoTime()) {
-      println("Still hogging the CPU")
-      spin(helloTime + Billion)
-    } else spin(helloTime)
+  def spin(helloTime: Long, endTime: Long): Unit = {
+    val now = System.nanoTime()
+    if (now < endTime) {
+      if (helloTime < now) {
+        println("Still hogging the CPU")
+        spin(helloTime + Billion, endTime)
+      } else { spin(helloTime, endTime) }
+    }
   }
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
     println("I am a CPU hog.")
-    spin(System.nanoTime() + Billion)
+    val now = System.nanoTime()
+    val hour = 60L * 60L * Billion
+    spin(now + Billion, now + hour)
   }
 }
