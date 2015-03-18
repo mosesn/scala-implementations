777bfa0635416d4007009de2d9278e986404d92d
Moses Nakamura <mnakamura@twitter.com>
2015-03-18 16:15:36 -0400

    finished up for comprehensions

--- src/main/scala/com/twitter/scala_impl/ForComprehension.scala
+++ src/main/scala/com/twitter/scala_impl/ForComprehension.scala
@@ -4,4 +4,15 @@ import scala.annotation.tailrec
 
 class ForComprehension {
   def foreach(seq: Seq[Int]): Unit = for { elt <- seq } println(elt)
+
+  def filter(seq: Seq[Int]): Unit = for { elt <- seq if elt % 2 == 0 } println(elt)
+
+  def map(seq: Seq[Int]): Seq[Int] = for { elt <- seq } yield elt
+
+  def map(seq: Seq[Int]): Seq[Int] = for { elt <- seq if elt % 2 == 0 } yield elt
+
+  def flatMap(seqOfSeq: Seq[Seq[Int]]): Seq[Int] = for {
+    seq <- seqOfSeq
+    elt <- seq
+  } yield elt
 }
